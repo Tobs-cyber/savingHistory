7367be602154bc214d568e1c8d47e359a3a5d0a6
Tobs GWNFT <58276582+Tobs-cyber@users.noreply.github.com>
2025-04-21 04:29:52 +0100

    first commit

---
+++ frontend/components/ComingSoon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ComingSoon = () => {
+  return (
+    <div className='my-20 py-20'>
+        <h1 className='text-6xl text-center font-bold my-10'>Coming Soon...</h1>
+    </div>
+  )
+}
+
+export default ComingSoon
