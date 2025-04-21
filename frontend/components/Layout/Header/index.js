e3644a89fc21b998537e3e69de6cdecbd1689c26
Tobs GWNFT <58276582+Tobs-cyber@users.noreply.github.com>
2025-04-21 04:24:14 +0100

    frontend UI

---
+++ frontend/components/Layout/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import HeaderOne from "./HeaderOne";
+import HeaderTwo from "./HeaderTwo";
+
+const Navbar = () => {
+  return (
+    <>
+      <HeaderOne />
+      <HeaderTwo />
+    </>
+  );
+};
+
+export default Navbar;
